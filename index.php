ee73504a7bc3fb4aa8cb872a420789357071f328
MuthiaDwirza <71259803+MuthiaDwirza@users.noreply.github.com>
2020-09-15 11:01:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-
+<html> 
+    <head> 
+        <title>Pratikum</title> 
+    </head> 
+    <body> <h2><u>BINTANG</u></h2>
+    <p>Muthia Dwirza Stefhanny</p>
+    <p>6701194125</p>
+    <p>D3SI 43-04</P>
+    <?php
+for ($a=1; $a<=5; $a++){
+    for ($b=5; $b>=$a; $b--){
+        echo "*";
+    }echo "<br>";
+}
 ?>
+</body> 
+</html>
+ 
